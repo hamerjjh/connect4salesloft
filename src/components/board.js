68abe186cdfdaf30b655b9215cd4d0f46fc33938
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2018-01-07 15:19:49 -0500

    created state for players and board

--- src/components/board.js
+++ src/components/board.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class Board extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+          player1: 1,
+          player2: 2,
+          currentPlayer: null,
+          board: [],
+        };
+        
+        // Bind play function to App component
+        this.play = this.play.bind(this);
+      }
     render() {
         return (
             <div>
