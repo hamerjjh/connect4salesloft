5da6ac9180068d83c97c68de5c5de8f064702f45
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2018-01-08 11:06:54 -0500

    check board for a draw

--- src/components/board.js
+++ src/components/board.js
@@ -121,6 +121,17 @@ class Board extends Component {
         }
       }
 
+      checkDraw(board) {
+        for (let r = 0; r < 6; r++) {
+          for (let c = 0; c < 7; c++) {
+            if (board[r][c] === null) {
+              return null;
+            }
+          }
+        }
+        return 'Awww its a draw :(';    
+      }
+
 
       componentWillMount() {
         this.initBoard();
