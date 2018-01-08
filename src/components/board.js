045c50fbf941101d38cd21824b8dd5dc92b4b483
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2018-01-08 11:04:59 -0500

    check diagonal right and left

--- src/components/board.js
+++ src/components/board.js
@@ -91,6 +91,36 @@ class Board extends Component {
         }
       }
 
+      checkDiagonalRight(board) {
+        // row is 3 or greater AND column is 3 or less
+        for (let r = 3; r < 6; r++) {
+          for (let c = 0; c < 4; c++) {
+            if (board[r][c]) {
+              if (board[r][c] === board[r - 1][c + 1] &&
+                  board[r][c] === board[r - 2][c + 2] &&
+                  board[r][c] === board[r - 3][c + 3]) {
+                return board[r][c];
+              }
+            }
+          }
+        }
+      }
+      
+      checkDiagonalLeft(board) {
+        // row is 3 or greater AND column is 3 or greater
+        for (let r = 3; r < 6; r++) {
+          for (let c = 3; c < 7; c++) {
+            if (board[r][c]) {
+              if (board[r][c] === board[r - 1][c - 1] &&
+                  board[r][c] === board[r - 2][c - 2] &&
+                  board[r][c] === board[r - 3][c - 3]) {
+                return board[r][c];
+              }
+            }
+          }
+        }
+      }
+
 
       componentWillMount() {
         this.initBoard();
